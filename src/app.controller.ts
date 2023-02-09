7839d090271f440d7395b59c61999860acbde9a7
Guan <2256618748@qq.com>
2023-02-09 17:24:41 +0800

    build: 构建chat-gpt应用

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('chat')
+  getChat(@Query('message') message): unknown {
+      return this.appService.getChatService(message);
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Configuration, OpenAIApi } from 'openai';
+
+@Injectable()
+export class AppService {
+  async getChatService(message: string): Promise<unknown> {
+    console.log(process.env.OPENAI_API_KEY);
+    console.log(message);
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    try {
+      const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: message,
+        max_tokens: 2048,
+        temperature: 1,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+      });
+      console.log(completion.data);
+      return {
+        msg: 'success',
+        code: '200',
+        result: {
+          id: completion.data.id,
+          created: completion.data.created,
+          text: completion.data.choices[0].text,
+        },
+      };
+    } catch (error) {
+      if (error.response) {
+        return {
+          msg: 'error',
+          code: error.response.status,
+          result: error.response.data,
+        };
+      } else {
+        return error.message;
+      }
+    }
+  }
+}
